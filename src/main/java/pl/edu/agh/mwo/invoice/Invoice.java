f3f69a6045dfd014297fd2c4adda5022b0591fb4
harych <harych@gmail.com>
2021-01-17 12:07:27 +0100

    Changed collection type from ArrayList to LinkedHashMap

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,51 +1,49 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
-
-	public Invoice() {
-		this.products = new ArrayList<Product>();
-	}
+	private Map<Product,Integer> products = new LinkedHashMap<>();
 
 	public void addProduct(Product product) {
-		this.products.add(product);
+		this.products.put(product, 1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
 		if (quantity <= 0) {
 			throw new IllegalArgumentException("Incorrect quantity");
-		}
-		for (int i = 0; i < quantity; i++) {
-			this.products.add(product);
+		} else {
+			this.products.put(product, quantity);
 		}
 	}
 
 	public BigDecimal getSubtotal() {
 		BigDecimal subtotal = BigDecimal.ZERO;
-		for (Product product : products) {
-			subtotal = subtotal.add(product.getPrice());
+		for (Product product : products.keySet()) {
+			BigDecimal quantity = new BigDecimal(products.get(product));
+			subtotal = subtotal.add(product.getPrice().multiply(quantity));
 		}
 		return subtotal;
 	}
 
 	public BigDecimal getTax() {
 		BigDecimal tax = BigDecimal.ZERO;
-		for (Product product : products) {
-			tax = tax.add(product.getPrice().multiply(product.getTaxPercent()));
+		for (Product product : products.keySet()) {
+			BigDecimal quantity = new BigDecimal(products.get(product));
+			tax = tax.add(product.getPrice().multiply(product.getTaxPercent()).multiply(quantity));
 		}
 		return tax;
 	}
 
 	public BigDecimal getTotal() {
 		BigDecimal total = BigDecimal.ZERO;
-		for (Product product : products) {
-			total = total.add(product.getPriceWithTax());
+		for (Product product : products.keySet()) {
+			BigDecimal quantity = new BigDecimal(products.get(product));
+			total = total.add(product.getPriceWithTax().multiply(quantity));
 		}
 		return total;
 	}
